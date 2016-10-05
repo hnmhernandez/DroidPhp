371ad98888ecfd184af7864e4d965eb78026f018
Harold Montenegro <hmontenegro@socialh4ck.com>
2016-10-05 07:28:36 -0400

    -limpiar codigo

--- droidPHP/src/main/java/org/opendroidphp/app/util/DroidPhpActivity.java
+++ droidPHP/src/main/java/org/opendroidphp/app/util/DroidPhpActivity.java
@@ -18,6 +18,8 @@ import android.view.WindowManager;
 import org.opendroidphp.app.NumpadTool;
 import org.opendroidphp.app.ui.FullscreenActivity;
 
+import static android.view.View.SYSTEM_UI_FLAG_FULLSCREEN;
+
 /**
  * Created by Harold Montenegro on 29/07/16.
  */
@@ -30,13 +32,22 @@ public class DroidPhpActivity extends Activity {
         preventStatusBarExpansion(this);
 
         if(Build.VERSION.SDK_INT > 11 && Build.VERSION.SDK_INT < 19) { // lower api
-            View v = this.getWindow().getDecorView();
-            v.setSystemUiVisibility(View.GONE);
+            View decorView = getWindow().getDecorView();
+            decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    | SYSTEM_UI_FLAG_FULLSCREEN
+                    | View.GONE);
         } else if(Build.VERSION.SDK_INT >= 19) {
             //for new api versions.
             View decorView = getWindow().getDecorView();
-            int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
-            decorView.setSystemUiVisibility(uiOptions);
+            decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    | SYSTEM_UI_FLAG_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
         }
     }
 
